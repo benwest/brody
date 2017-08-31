f00322eba373e4fa5cce8bc705dd2ada1616fd97
Ben West <ben.s.west@gmail.com>
2017-08-31 19:34:24 +0000

    stopped blocking new tabs

--- js/view/Layout.js
+++ js/view/Layout.js
@@ -5,29 +5,6 @@ var classnames = require('classnames');
 var Menu = require('./menu/Menu');
 var MenuButton = require('./menu/MenuButton');
 
-// var styles = j2c.attach({
-    
-//     '.main': {
-        
-//         // position: 'fixed',
-//         // top: 0,
-//         // left: 0,
-//         // width: '100%',
-//         // height: '100%',
-//         // transition: 'transform .5s, opacity .5s',
-//         // background: 'black',
-//         // overflowX: 'hidden',
-//         // overflowY: 'scroll',
-        
-//         // '&_menuOpen': {
-//         //     transform: 'scale(1.1, 1.1)',
-//         //     opacity: 0
-//         // }
-        
-//     }
-    
-// })
-
 module.exports = {
     
     oninit: ({ state }) => {
@@ -51,6 +28,7 @@ module.exports = {
         var toggleMenu = () => state.menuOpen = !state.menuOpen;
         
         var link = ({ dom }) => dom.addEventListener('click', e => {
+            if (e.ctrlKey || e.metaKey || e.shiftKey || e.which === 2) return;
             e.preventDefault();
             state.menuOpen = false;
             scrollTo( 0, 0 );
