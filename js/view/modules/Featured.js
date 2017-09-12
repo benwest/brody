a097fe89e288ca1fd0a3089ad7cecb64ae52db2b
Ben West <ben.s.west@gmail.com>
2017-09-12 11:30:39 +0000

    back to dom

--- js/view/modules/Featured.js
+++ js/view/modules/Featured.js
@@ -1,143 +1,35 @@
 var m = require('mithril');
-var Fixer = require('./Fixer');
-var Rects = require('../modules/Rects');
-var rect = require('../hero/rects');
-var viewport = require('../utils/viewport');
-var { margin } = require( '../utils/metrics' );
-
-var List = {
-    
-    render: ({
-        uniforms: { position, size, scrollTop, color },
-        children
-    }) => {
-        
-        return rect( { position, size, color, clip: true },
-            rect( { position: [ 0, -scrollTop ], size: [ 0, 0 ] },
-                children
-            )
-        )
-        
-    }
-    
-}
-
-var listModel = ( title, children ) => rect( List,
-    {
-        title: {
-            text: title,
-            y: 0
-        },
-        position: [ 0, 0 ],
-        size: [ 0, 0 ],
-        color: [ 0, 0, 0 ],
-        scrollTop: 0,
-    },
-    children
-)
-
-var color = ( list, depth = 0 ) => {
-    
-    var v = ( ( depth * 2 + 2 ) * 16 ) / 255;
-    
-    list.uniforms.color = [ v, v, v ];
-    
-    list.children.forEach( child => color( child, depth + 1 ) )
-    
-}
-
-var layout = ( item, parentSize, margin, i = 0 ) => {
-    
-    item.uniforms.size = [
-        parentSize[ 0 ] - ( margin[ 1 ] + margin[ 3 ] ),
-        parentSize[ 1 ] - ( margin[ 0 ] + margin[ 2 ] )
-    ];
-    
-    item.uniforms.position = [
-        margin[ 3 ],
-        parentSize[ 1 ] * i + margin[ 0 ]
-    ];
-    
-    item.children.forEach( ( child, i ) => {
-        
-        layout( child, item.uniforms.size, margin, i )
-        
-    });
-    
-}
-
-var scrollHeight = list => {
-    
-    var ownHeight = list.uniforms.size[ 1 ] * ( Math.max( list.children.length, 1 ) - 1 );
-    
-    var childHeight = list.children
-        .filter( item => item.component === List )
-        .reduce( ( total, item ) => total + scrollHeight( item ), 0 );
-    
-    return ownHeight + childHeight;
-    
-}
-
-var scroll = ( list, top ) => {
-    
-    list.uniforms.scrollTop = 0;
-    
-    var l = list.children.length;
-    
-    list.children.forEach( ( item, i ) => {
-        
-        if ( item.component === List ) {
-            
-            top = scroll( item, top );
-            
-        }
-        
-        if ( i < l - 1 ) {
-            
-            var d = Math.min( list.uniforms.size[ 1 ], top );
-            list.uniforms.scrollTop += d;
-            top -= d;
-        
-        }
-        
-    });
-    
-    return top;
-    
-};
+var Frames = require('../frames/Frames');
 
 module.exports = {
     
-    view: ({ attrs: { disciplines } }) => {
+    oninit: ({ state, attrs: { disciplines } }) => {
         
-        var structure = listModel( 'root',
-            disciplines.map( discipline => {
-                return listModel( discipline.name,
-                    discipline.projects.map( project => {
-                        return listModel( project.title, [] )
-                    })
-                )
-            })
-        )
-        
-        color( structure );
-        
-        layout( structure, [ viewport.w, viewport.h ], margin() );
-        
-        var getContent = top => {
-            
-            scroll( structure, top );
-            
-            return <Rects root={ structure }/>;
-            
+        state.structure = {
+            type: 'list',
+            attrs: {
+                title: 'root',
+                children: disciplines.map( discipline => ({
+                    type: 'list',
+                    attrs: {
+                        title: discipline.name,
+                        children: discipline.projects.map( project => ({
+                            type: 'item',
+                            attrs: {
+                                title: project.title,
+                                content: project.hero
+                            }
+                        }) )
+                    }
+                }) )
+            }
         }
         
-        return <Fixer
-            width={ viewport.w }
-            height={ viewport.h }
-            scrollHeight={ scrollHeight( structure ) }
-            getContent={ getContent }
-        />
+    },
+    
+    view: ({ state: { structure }}) => {
+        
+        return <Frames structure={ structure }/>
         
     }
     
