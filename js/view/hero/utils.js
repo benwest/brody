29af2c6c16ae37916b15fd9fcae046fe9cb592ef
Ben West <ben.s.west@gmail.com>
2017-09-12 11:33:22 +0000

    bye heroes

--- js/view/hero/utils.js
+++
@@ -1,3 +0,0 @@
-var resolution = ({drawingBufferWidth, drawingBufferHeight}) => [ drawingBufferWidth, drawingBufferHeight ];
-
-module.exports = { resolution };
--- js/view/hero/utils/fit.js
+++
@@ -1,7 +0,0 @@
-module.exports = ( out, [ srcW, srcH ], [ destW, destH ], fit = 'cover' ) => {
-    var fn = fit === 'contain' ? Math.min : Math.max;
-    var scale = fn( destW / srcW, destH / srcH );
-    out[ 0 ] = srcW * scale;
-    out[ 1 ] = srcH * scale;
-    return out;
-}
